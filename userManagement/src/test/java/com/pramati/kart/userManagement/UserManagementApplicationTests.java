99d06e55eaaef3d28175303cc01336b58d4910a2
priyankt <priyanktripathi007@gmail.com>
2019-10-18 14:05:18 +0530

    pusing the user management project

---
+++ userManagement/src/test/java/com/pramati/kart/userManagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramati.kart.userManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
